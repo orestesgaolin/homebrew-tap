593a5d44810410dffd93fb849d55b6fdf37b96ba
Dominik Roszkowski <dominik@roszkowski.dev>
2024-10-25 01:24:00 +0200

    chore(changelog_cli): update to 0.5.1

--- changelog_cli.rb
+++ changelog_cli.rb
@@ -3,8 +3,8 @@ require "yaml"
 class ChangelogCli < Formula
   desc "A simple opinionated CLI to generate changelog based on the conventional commit history."
   homepage "https://github.com/orestesgaolin/dart_utilities"
-  url "https://github.com/orestesgaolin/dart_utilities/archive/refs/tags/changelog_cli-v0.4.0.tar.gz"
-  sha256 "a62726f5ea9788ae12ae64ef073c932dd8713be9def75a405992f22267e0c20d"
+  url "https://github.com/orestesgaolin/dart_utilities/archive/refs/tags/changelog_cli-v0.5.1.tar.gz"
+  sha256 "1cf8469c262791fd1ed780f0f4ca6c24835ac56abb8845efed08ea2592212c97"
   license "MIT"
 
   depends_on "dart-lang/dart/dart" => :build
