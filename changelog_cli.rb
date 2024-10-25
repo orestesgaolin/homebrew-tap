e576624a1483a73b06244e5fc05be2952d9835c3
Dominik Roszkowski <dominik@roszkowski.dev>
2024-10-25 10:23:38 +0200

    chore: experiment with binary files

--- changelog_cli.rb
+++ changelog_cli.rb
@@ -1,50 +1,33 @@
-require "yaml"
-
 class ChangelogCli < Formula
   desc "A simple opinionated CLI to generate changelog based on the conventional commit history."
   homepage "https://github.com/orestesgaolin/dart_utilities"
-  url "https://github.com/orestesgaolin/dart_utilities/archive/refs/tags/changelog_cli-v0.5.1.tar.gz"
-  sha256 "1cf8469c262791fd1ed780f0f4ca6c24835ac56abb8845efed08ea2592212c97"
+  # url "https://github.com/orestesgaolin/dart_utilities/archive/refs/tags/changelog_cli-v0.5.1.tar.gz"
+  # sha256 "1cf8469c262791fd1ed780f0f4ca6c24835ac56abb8845efed08ea2592212c97"
   license "MIT"
 
-  depends_on "dart-lang/dart/dart" => :build
-
   def install
-    # Tell the pub server where these installations are coming from.
-    ENV["PUB_ENVIRONMENT"] = "homebrew:changelog_cli"
+    binary_name = case OS.mac?
+                  when true
+                    Hardware::CPU.intel? ? "changelog_cli_macos_x86" : "changelog_cli_macos"
+                  else
+                    OS.linux? ? "changelog_cli_linux" : "changelog_cli.exe"
+                  end
+                  
+    binary_url = "https://github.com/orestesgaolin/dart_utilities/releases/download/changelog_cli-v0.5.1/#{binary_name}"
+    binary_sha256 = case binary_name
+                    when "changelog_cli_macos" then "39ea8af23356b89ba2d9deef936dc8fa3532d90353710b2793cc5f9c84925ea1"
+                    when "changelog_cli_macos_x86" then "620691f7e4a5cc95249804413e28e6f57fc198890f92e72e4ad610aaa58cd7d6"
+                    when "changelog_cli_linux" then "8d00fbd45ab672bebe7d632df881fd23f0cc5fd6bcd839610ba0a32197a7ff01"
+                    when "changelog_cli.exe" then "74403174a30aab9040415b6b1489a067f1e2dc06c63e806e10b5ec71aee7df34"
+                    end
 
-    # Change directories into the changelog_cli package directory.
-    Dir.chdir('changelog_cli')
-
-    system _dart/"dart", "pub", "get"
-  
-    _install_script_snapshot
-
-    chmod 0555, "#{bin}/changelog_cli"
+    # Download the appropriate binary for the system
+    system "curl", "-L", binary_url, "-o", "changelog_cli"
+    chmod 0555, "changelog_cli"
+    bin.install "changelog_cli"
   end
 
-  private
-
-  def _dart
-    @_dart ||= Formula["dart-lang/dart/dart"].libexec/"bin"
-  end
-
-  def _version
-    @_version ||= YAML.safe_load(File.read("pubspec.yaml"))["version"]
-  end
-
-  def _install_script_snapshot
-    system _dart/"dart", "compile", "jit-snapshot",
-           "-Dversion=#{_version}",
-           "-o", "changelog_cli.dart.app.snapshot",
-           "bin/changelog_cli.dart"
-    lib.install "changelog_cli.dart.app.snapshot"
-    
-    cp _dart/"dart", lib
-
-    (bin/"changelog_cli").write <<~SH
-      #!/bin/sh
-      exec "#{lib}/dart" "#{lib}/changelog_cli.dart.app.snapshot" "$@"
-    SH
+  test do
+    system "#{bin}/changelog_cli", "--version"
   end
 end
