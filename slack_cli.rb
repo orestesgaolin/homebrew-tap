f8d4e763d34c77ded563debe74cfc45703fee1b9
Dominik Roszkowski <dominik@roszkowski.dev>
2025-11-10 16:31:13 +0100

    chore(slack_cli): simplify installation process and remove unused dependencies

--- slack_cli.rb
+++ slack_cli.rb
@@ -4,47 +4,32 @@ class SlackCli < Formula
   desc "A simple opinionated CLI to send messages to Slack."
   homepage "https://github.com/orestesgaolin/dart_utilities"
   url "https://github.com/orestesgaolin/dart_utilities/archive/refs/tags/slack_cli-v0.2.1.tar.gz"
-  sha256 "2d9a5b8ba6e9234d45044fd388c9fcb8973a5f6c153e7ed2357d1f0bf2d3eb4e"
+  sha256 ""
   license "MIT"
 
-  depends_on "dart-lang/dart/dart" => :build
-
   def install
-    # Tell the pub server where these installations are coming from.
-    ENV["PUB_ENVIRONMENT"] = "homebrew:slack_cli"
+    binary_name = case OS.mac?
+                  when true
+                    Hardware::CPU.intel? ? "slack_cli_macos_x86" : "slack_cli_macos"
+                  else
+                    OS.linux? ? "slack_cli_linux" : "slack_cli.exe"
+                  end
+                  
+    binary_url = "https://github.com/orestesgaolin/dart_utilities/releases/download/slack_cli-v0.2.1/#{binary_name}"
+    binary_sha256 = case binary_name
+                    when "slack_cli_macos" then ""
+                    when "slack_cli_macos_x86" then ""
+                    when "slack_cli_linux" then ""
+                    when "slack_cli.exe" then ""
+                    end
 
-    # Change directories into the slack_cli package directory.
-    Dir.chdir('slack_cli')
-
-    system _dart/"dart", "pub", "get"
-  
-    _install_script_snapshot
-
-    chmod 0555, "#{bin}/slack_cli"
+    # Use full path to avoid conflicts
+    system "curl", "-L", binary_url, "-o", "#{buildpath}/slack_cli_bin"
+    chmod 0555, "#{buildpath}/slack_cli_bin"
+    bin.install "#{buildpath}/slack_cli_bin" => "slack_cli"
   end
 
-  private
-
-  def _dart
-    @_dart ||= Formula["dart-lang/dart/dart"].libexec/"bin"
-  end
-
-  def _version
-    @_version ||= YAML.safe_load(File.read("pubspec.yaml"))["version"]
-  end
-
-  def _install_script_snapshot
-    system _dart/"dart", "compile", "jit-snapshot",
-           "-Dversion=#{_version}",
-           "-o", "slack_cli.dart.app.snapshot",
-           "bin/slack_cli.dart"
-    lib.install "slack_cli.dart.app.snapshot"
-    
-    cp _dart/"dart", lib
-
-    (bin/"slack_cli").write <<~SH
-      #!/bin/sh
-      exec "#{lib}/dart" "#{lib}/slack_cli.dart.app.snapshot" "$@"
-    SH
+  test do
+    system "#{bin}/slack_cli", "--version"
   end
 end
